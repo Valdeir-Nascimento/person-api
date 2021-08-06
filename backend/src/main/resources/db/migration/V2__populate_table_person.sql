878aa64a87595508195ab3d1c1a0ac59431594c7
STEFANINI-DOM\vcnascimento1 <valdeircn11@gmail.com>
2021-08-06 20:43:23 -0300

    Adcionando Flyway ao projeto

---
+++ backend/src/main/resources/db/migration/V2__populate_table_person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(2, 'Itapuã setor III BL 11A AP 101', 'Maria', 'Female', 'Edna'),
+	(3, 'Rio Volga setor IV BL 13A AP 201', 'Elton', 'Male', 'Cesar'),
+	(4, 'Itapuã setor BL 14A AP 102', 'Adriana', 'Female', 'Oliveira'),
+	(5, 'Rua Tapanã, 203', 'Raimundo', 'Male', 'Nonato'),
+	(6, 'Residencial Rio Volgar Setor II BL 11A AP 101', 'Valdivia', 'Male', 'Carneiro');
